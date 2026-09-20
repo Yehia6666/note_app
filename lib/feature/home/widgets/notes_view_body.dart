57eb526edf8eea3112756ceadb75bb240abd3fc4
Yehia6666 <yehia01128445409@gmail.com>
2026-09-20 22:30:01 +0300

    create custom note icon

--- lib/feature/home/widgets/notes_view_body.dart
+++ lib/feature/home/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/feature/home/widgets/custom_appbar.dart';
+import 'package:note_app/feature/home/widgets/note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -8,9 +9,10 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24 ,vertical: 24),
-      child: Column(
+      child: ListView(
         children: [
           CustomAppbar(),
+          NoteItem(),
         ],
       ),
     );
