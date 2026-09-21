22f709535c584aceaa206ba97a4be716b356f5ad
Yehia6666 <yehia01128445409@gmail.com>
2026-09-21 15:49:29 +0300

    create edit note view

--- lib/feature/home/widgets/note_item.dart
+++ lib/feature/home/widgets/note_item.dart
@@ -1,51 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/feature/home/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      margin: EdgeInsets.only(top: 12),
-      decoration: BoxDecoration(
-        color: const Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.zero,
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 16),
-              child: const Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        margin: EdgeInsets.only(top: 12),
+        decoration: BoxDecoration(
+          color: const Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: const Text(
+                  'Flutter Tips',
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                ),
+              ),
+              subtitle: Text(
+                'build your career with Tharwat Samy',
+                style: TextStyle(color: Colors.grey[800], fontSize: 18),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
               ),
             ),
-            subtitle: Text(
-              'build your career with Tharwat Samy',
-              style: TextStyle(color: Colors.grey[800], fontSize: 18),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const FaIcon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
+            Padding(
+              padding: const EdgeInsets.only(right: 16, top: 12),
+              child: Text(
+                'May 21,2026',
+                style: TextStyle(color: Colors.grey[800]),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16, top: 12),
-            child: Text(
-              'May 21,2026',
-              style: TextStyle(color: Colors.grey[800]),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
